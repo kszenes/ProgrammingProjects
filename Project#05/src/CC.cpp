c12ea946a735b0d3cb6f64cfe552824b2276d05d
Kalman Szenes <szenes.kalman@gmail.com>
2024-05-11 11:13:05 +0200

    Fixed indexing, MP2 working

--- Project#05/src/CC.cpp
+++ Project#05/src/CC.cpp
@@ -6,24 +6,24 @@ Eigen::VectorXd CC::get_eri_mo(const bool fast_algo) const {
   Eigen::VectorXd eri_mo = Eigen::VectorXd::Zero(ints_.get_eri().size());
   const Eigen::MatrixXd &coeffs = scf_.Coeffs;
 
-  const int n_ao = scf_.n_ao;
   double val;
   if (fast_algo) {
 #define INDEX(mu, nu, lambda, sigma)                                           \
-  mu + nu *n_ao + lambda *n_ao *n_ao + sigma *n_ao *n_ao *n_ao
+  mu + nu *scf_.n_ao + lambda *scf_.n_ao *scf_.n_ao +                          \
+      sigma *scf_.n_ao *scf_.n_ao *scf_.n_ao
 
-    std::vector<double> tmp_s(std::pow(n_ao, 4), 0);
-    std::vector<double> tmp_r(std::pow(n_ao, 4), 0);
-    std::vector<double> tmp_q(std::pow(n_ao, 4), 0);
+    std::vector<double> tmp_s(std::pow(scf_.n_ao, 4), 0);
+    std::vector<double> tmp_r(std::pow(scf_.n_ao, 4), 0);
+    std::vector<double> tmp_q(std::pow(scf_.n_ao, 4), 0);
     double val = 0.0;
 
     // (mu, nu, lambda, sigma) -> (mu, nu, lambda, s)
-    for (int p = 0; p < n_ao; ++p) {
-      for (int q = 0; q < n_ao; ++q) {
-        for (int r = 0; r < n_ao; ++r) {
-          for (int s = 0; s < n_ao; ++s) {
+    for (int p = 0; p < scf_.n_ao; ++p) {
+      for (int q = 0; q < scf_.n_ao; ++q) {
+        for (int r = 0; r < scf_.n_ao; ++r) {
+          for (int s = 0; s < scf_.n_ao; ++s) {
             val = 0.0;
-            for (int sigma = 0; sigma < n_ao; ++sigma) {
+            for (int sigma = 0; sigma < scf_.n_ao; ++sigma) {
               val += coeffs(sigma, s) *
                      ints_.get_eri()(get_4index(p, q, r, sigma));
             }
@@ -34,12 +34,12 @@ Eigen::VectorXd CC::get_eri_mo(const bool fast_algo) const {
     }
 
     // (mu, nu, lambda, s) -> (mu, nu, r, s)
-    for (int p = 0; p < n_ao; ++p) {
-      for (int q = 0; q < n_ao; ++q) {
-        for (int r = 0; r < n_ao; ++r) {
-          for (int s = 0; s < n_ao; ++s) {
+    for (int p = 0; p < scf_.n_ao; ++p) {
+      for (int q = 0; q < scf_.n_ao; ++q) {
+        for (int r = 0; r < scf_.n_ao; ++r) {
+          for (int s = 0; s < scf_.n_ao; ++s) {
             val = 0.0;
-            for (int lambda = 0; lambda < n_ao; ++lambda) {
+            for (int lambda = 0; lambda < scf_.n_ao; ++lambda) {
               val += coeffs(lambda, r) * tmp_s[INDEX(p, q, lambda, s)];
             }
             tmp_r[INDEX(p, q, r, s)] = val;
@@ -49,12 +49,12 @@ Eigen::VectorXd CC::get_eri_mo(const bool fast_algo) const {
     }
 
     // (mu, nu, r, s) -> (mu, q, r, s)
-    for (int p = 0; p < n_ao; ++p) {
-      for (int q = 0; q < n_ao; ++q) {
-        for (int r = 0; r < n_ao; ++r) {
-          for (int s = 0; s < n_ao; ++s) {
+    for (int p = 0; p < scf_.n_ao; ++p) {
+      for (int q = 0; q < scf_.n_ao; ++q) {
+        for (int r = 0; r < scf_.n_ao; ++r) {
+          for (int s = 0; s < scf_.n_ao; ++s) {
             val = 0.0;
-            for (int nu = 0; nu < n_ao; ++nu) {
+            for (int nu = 0; nu < scf_.n_ao; ++nu) {
               val += coeffs(nu, q) * tmp_r[INDEX(p, nu, r, s)];
             }
             tmp_q[INDEX(p, q, r, s)] = val;
@@ -64,12 +64,12 @@ Eigen::VectorXd CC::get_eri_mo(const bool fast_algo) const {
     }
 
     // (mu, q, r, s) -> (p, q, r, s)
-    for (int p = 0; p < n_ao; ++p) {
-      for (int q = 0; q < n_ao; ++q) {
-        for (int r = 0; r < n_ao; ++r) {
-          for (int s = 0; s < n_ao; ++s) {
+    for (int p = 0; p < scf_.n_ao; ++p) {
+      for (int q = 0; q < scf_.n_ao; ++q) {
+        for (int r = 0; r < scf_.n_ao; ++r) {
+          for (int s = 0; s < scf_.n_ao; ++s) {
             val = 0.0;
-            for (int mu = 0; mu < n_ao; ++mu) {
+            for (int mu = 0; mu < scf_.n_ao; ++mu) {
               val += coeffs(mu, p) * tmp_q[INDEX(mu, q, r, s)];
             }
             eri_mo(get_4index(p, q, r, s)) = val;
@@ -80,16 +80,16 @@ Eigen::VectorXd CC::get_eri_mo(const bool fast_algo) const {
 
   } else {
     // MO indices
-    for (int p = 0; p < n_ao; ++p) {
-      for (int q = 0; q < n_ao; ++q) {
-        for (int r = 0; r < n_ao; ++r) {
-          for (int s = 0; s < n_ao; ++s) {
+    for (int p = 0; p < scf_.n_ao; ++p) {
+      for (int q = 0; q < scf_.n_ao; ++q) {
+        for (int r = 0; r < scf_.n_ao; ++r) {
+          for (int s = 0; s < scf_.n_ao; ++s) {
             // AO indices
             val = 0;
-            for (int mu = 0; mu < n_ao; ++mu) {
-              for (int nu = 0; nu < n_ao; ++nu) {
-                for (int lambda = 0; lambda < n_ao; ++lambda) {
-                  for (int sigma = 0; sigma < n_ao; ++sigma) {
+            for (int mu = 0; mu < scf_.n_ao; ++mu) {
+              for (int nu = 0; nu < scf_.n_ao; ++nu) {
+                for (int lambda = 0; lambda < scf_.n_ao; ++lambda) {
+                  for (int sigma = 0; sigma < scf_.n_ao; ++sigma) {
                     val += coeffs(mu, p) * coeffs(nu, q) *
                            ints_.get_eri()(get_4index(mu, nu, lambda, sigma)) *
                            coeffs(lambda, r) * coeffs(sigma, s);
@@ -108,11 +108,10 @@ Eigen::VectorXd CC::get_eri_mo(const bool fast_algo) const {
 }
 
 Eigen::MatrixXd CC::spatial2spin_1b(const Eigen::MatrixXd &mat_in) const {
-  const int n_ao = scf_.n_ao;
-  Eigen::MatrixXd mat_out(2 * n_ao, 2 * n_ao);
+  Eigen::MatrixXd mat_out(n_ao, n_ao);
 
-  for (int p = 0; p < n_ao; ++p) {
-    for (int q = 0; q < n_ao; ++q) {
+  for (int p = 0; p < scf_.n_ao; ++p) {
+    for (int q = 0; q < scf_.n_ao; ++q) {
       mat_out(2 * p, 2 * q) = mat_in(p, q);
       mat_out(2 * p + 1, 2 * q + 1) = mat_in(p, q);
     }
@@ -122,13 +121,12 @@ Eigen::MatrixXd CC::spatial2spin_1b(const Eigen::MatrixXd &mat_in) const {
 
 Eigen::Tensor<double, 4>
 CC::spatial2spin_2b(const Eigen::VectorXd &tensor_in) const {
-  const int n_ao = scf_.n_ao;
   // Spin basis
-  Eigen::Tensor<double, 4> tensor_out(2 * n_ao, 2 * n_ao, 2 * n_ao, 2 * n_ao);
-  for (int p = 0; p < n_ao; ++p) {
-    for (int q = 0; q < n_ao; ++q) {
-      for (int r = 0; r < n_ao; ++r) {
-        for (int s = 0; s < n_ao; ++s) {
+  Eigen::Tensor<double, 4> tensor_out(n_ao, n_ao, n_ao, n_ao);
+  for (int p = 0; p < scf_.n_ao; ++p) {
+    for (int q = 0; q < scf_.n_ao; ++q) {
+      for (int r = 0; r < scf_.n_ao; ++r) {
+        for (int s = 0; s < scf_.n_ao; ++s) {
           // Mulliken (p, r, q, s) -> <p, q, r, s> Physics
 
           // <up, up, up, up>
@@ -150,9 +148,23 @@ CC::spatial2spin_2b(const Eigen::VectorXd &tensor_in) const {
   return tensor_out;
 }
 
+Eigen::Tensor<double, 4>
+CC::antisymmetrise(const Eigen::Tensor<double, 4> &tensor_in) const {
+  Eigen::Tensor<double, 4> tensor_out(n_ao, n_ao, n_ao, n_ao);
+  for (int p = 0; p < n_ao; ++p) {
+    for (int q = 0; q < n_ao; ++q) {
+      for (int r = 0; r < n_ao; ++r) {
+        for (int s = 0; s < n_ao; ++s) {
+          tensor_out(p, q, r, s) =
+              tensor_in(p, q, r, s) - tensor_in(p, q, s, r);
+        }
+      }
+    }
+  }
+  return tensor_out;
+}
+
 Eigen::MatrixXd CC::get_fock_spin() const {
-  int n_ao = 2 * scf_.n_ao;
-  int n_occ = 2 * scf_.n_occ;
   Eigen::MatrixXd h_pq = spatial2spin_1b(ints_.get_T() + ints_.get_V());
   Eigen::MatrixXd fock_spin = Eigen::MatrixXd::Zero(n_ao, n_ao);
 
@@ -161,7 +173,7 @@ Eigen::MatrixXd CC::get_fock_spin() const {
     for (int q = 0; q < n_ao; ++q) {
       val = 0.0;
       for (int m = 0; m < n_occ; ++m) {
-        val += eri(p, m, q, m) - eri(p, m, m, q);
+        val += eri_anti(p, m, q, m);
       }
       fock_spin(p, q) = val;
     }
@@ -170,10 +182,6 @@ Eigen::MatrixXd CC::get_fock_spin() const {
 }
 
 void CC::init_amplitudes() {
-  int n_ao = 2 * scf_.n_ao;
-  int n_occ = 2 * scf_.n_occ;
-  int n_virtual = n_ao - n_occ;
-
   Eigen::VectorXd mo_e_space = scf_.get_mo_energies();
   Eigen::VectorXd mo_e_spin = Eigen::VectorXd::Zero(n_ao);
   for (int i = 0; i < n_ao / 2; ++i) {
@@ -182,16 +190,15 @@ void CC::init_amplitudes() {
   }
 
   t1 = Eigen::MatrixXd::Zero(n_occ, n_virtual);
-
   t2 = Eigen::Tensor<double, 4>(n_occ, n_occ, n_virtual, n_virtual);
 
   for (int i = 0; i < n_occ; ++i) {
     for (int j = 0; j < n_occ; ++j) {
-      for (int a = n_occ; a < n_ao; ++a) {
-        for (int b = n_occ; b < n_ao; ++b) {
-          double numer = eri(i, j, a, b) - eri(i, j, b, a);
-          double denom =
-              mo_e_spin(i) + mo_e_spin(j) - mo_e_spin(a) - mo_e_spin(b);
+      for (int a = 0; a < n_virtual; ++a) {
+        for (int b = 0; b < n_virtual; ++b) {
+          double numer = eri_anti(i, j, a + n_occ, b + n_occ);
+          double denom = mo_e_spin(i) + mo_e_spin(j) - mo_e_spin(a + n_occ) -
+                         mo_e_spin(b + n_occ);
           t2(i, j, a, b) = numer / denom;
         }
       }
@@ -200,30 +207,50 @@ void CC::init_amplitudes() {
 }
 
 void CC::compute_mp2_e() const {
-  int n_ao = 2 * scf_.n_ao;
-  int n_occ = 2 * scf_.n_occ;
-
   double mp2_e = 0.0;
   for (int i = 0; i < n_occ; ++i) {
     for (int j = 0; j < n_occ; ++j) {
-      for (int a = n_occ; a < n_ao; ++a) {
-        for (int b = n_occ; b < n_ao; ++b) {
-          mp2_e += (eri(i, j, a, b) - eri(i, j, b, a)) * t2(i, j, a, b);
+      for (int a = 0; a < n_virtual; ++a) {
+        for (int b = 0; b < n_virtual; ++b) {
+          mp2_e += (eri_anti(i, j, a + n_occ, b + n_occ)) * t2(i, j, a, b);
         }
       }
     }
   }
   mp2_e = mp2_e / 4.0;
-  fmt::println("MP2 Energy: {}\n", mp2_e);
+  fmt::println("MP2 Energy using Amplitudes: {}\n", mp2_e);
 }
+
+void CC::build_taus() {
+  tau.resize(n_occ, n_occ, n_virtual, n_virtual);
+  tau_tilde.resize(n_occ, n_occ, n_virtual, n_virtual);
+
+  for (int i = 0; i < n_occ; ++i) {
+    for (int j = 0; j < n_occ; ++j) {
+      for (int a = 0; a < n_virtual; ++a) {
+        for (int b = 0; b < n_virtual; ++b) {
+          tau_tilde(i, j, a, b) = t2(i, j, a, b) + 0.5 * (t1(i, a) * t1(j, b) -
+                                                          t1(i, b) * t1(j, a));
+          tau(i, j, a, b) =
+              t2(i, j, a, b) + t1(i, a) * t1(j, b) - t1(i, b) * t1(j, a);
+        }
+      }
+    }
+  }
+}
+
+void CC::build_intermediates() {}
 
 void CC::prepare() {
   Eigen::VectorXd eri_spatial = get_eri_mo();
-  eri = spatial2spin_2b(eri_spatial);
+  eri_anti = antisymmetrise(spatial2spin_2b(eri_spatial));
   fock = get_fock_spin();
+
   init_amplitudes();
 
   compute_mp2_e();
+
+  build_taus();
 }
 
 void CC::run() {
--- Project#05/src/CC.h
+++ Project#05/src/CC.h
@@ -8,7 +8,9 @@
 
 class CC {
 public:
-  CC(const IntegralParser &ints, const SCF &scf) : ints_(ints), scf_(scf) {}
+  CC(const IntegralParser &ints, const SCF &scf)
+      : n_ao(2 * scf.n_ao), n_occ(2 * scf.n_occ),
+        n_virtual(2 * (scf.n_ao - scf.n_occ)), ints_(ints), scf_(scf) {}
   void run();
 
 private:
@@ -16,18 +18,35 @@ private:
   Eigen::MatrixXd spatial2spin_1b(const Eigen::MatrixXd &mat_in) const;
   Eigen::Tensor<double, 4>
   spatial2spin_2b(const Eigen::VectorXd &tensor_in) const;
+  Eigen::Tensor<double, 4>
+  antisymmetrise(const Eigen::Tensor<double, 4> &tensor_in) const;
   Eigen::MatrixXd get_fock_spin() const;
   void init_amplitudes();
   void compute_mp2_e() const;
+  void build_taus();
+  void build_intermediates();
 
   void prepare();
 
+  // in spin orbitals
+  const int n_ao;
+  const int n_occ;
+  const int n_virtual;
+
   IntegralParser ints_;
   SCF scf_;
-  Eigen::Tensor<double, 4> eri; // in spin basis
-  Eigen::MatrixXd fock;         // in spin basis
+  Eigen::Tensor<double, 4> eri_anti; // <pq||rs> antsimmetrised in spin basis
+  Eigen::MatrixXd fock;              // in spin basis
+                                     //
+  // Ampllitudes
   Eigen::MatrixXd t1;
   Eigen::Tensor<double, 4> t2;
+  // Effective two particle excitation operators
+  Eigen::Tensor<double, 4> tau;
+  Eigen::Tensor<double, 4> tau_tilde;
+  // Intermediates
+  Eigen::MatrixXd F;
+  Eigen::Tensor<double, 4> W;
 };
 
 #endif // CC_H
